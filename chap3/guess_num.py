df511630b9cd2ce1ac8d68df951fe27b0d488827
Ziggy <zkgatu@gmail.com>
2014-10-03 12:34:12 +0100

    Modified game so that the player only has 5 attempts at guessing

--- chap3/guess_num.py
+++ chap3/guess_num.py
@@ -4,7 +4,7 @@ import random
 
 print("\nWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and 100")
-print("Try to guess it in as few attempts as possible.\n")
+print("You get 5 attempts at guessing.\n")
 
 # set the initial values
 the_number = random.randint(1, 100)
@@ -12,7 +12,7 @@ guess = int(input("Take a guess: "))
 tries = 1
 
 # guessing loop
-while guess != the_number:
+while guess != the_number and tries < 5:
   if guess > the_number:
     print("Lower ...")
   else:
@@ -22,7 +22,10 @@ while guess != the_number:
   guess = int(input("Take a guess: "))
   tries += 1
 
-print("You guessed it!  The number was", the_number)
-print("And it only took you", tries, "tries!\n")
+if guess != the_number:
+  print("Sorry, you didn't guess the number in", tries, "tries. Game over!")
+else:
+  print("You guessed it!  The number was", the_number)
+  print("And it only took you", tries, "tries!\n")
 
 input("\n\nPress the enter key to exit")
